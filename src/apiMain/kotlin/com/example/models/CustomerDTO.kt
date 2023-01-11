048ab890ad314c0a2be510852c4c72b177e7d2d5
yasuaki640 <yasuaki640@gmail.com>
2023-01-11 22:49:48 +0900

    a:efkae:f;k

--- src/apiMain/kotlin/com/example/models/CustomerDTO.kt
+++ src/apiMain/kotlin/com/example/models/CustomerDTO.kt
@@ -1,6 +1,9 @@
 package com.example.models
 
+import com.example.Customer
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class CustomerDTO(val id: Long, val firstName: String, val lastName: String, val email: String)
+data class CustomerDTO(val id: Long = 0, val firstName: String, val lastName: String, val email: String)
+
+fun CustomerDTO.toEntity() = Customer(this.id, this.firstName, this.lastName, this.email)
--- src/apiMain/kotlin/com/example/routes/CustomerRoutes.kt
+++ src/apiMain/kotlin/com/example/routes/CustomerRoutes.kt
@@ -2,18 +2,31 @@ package com.example.routes
 
 import com.example.Database
 import com.example.models.CustomerDTO
+import com.example.models.toEntity
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.customerRouting(db: Database) {
-    val mapper =
+    val toDto =
         { id: Long, firstName: String, lastName: String, email: String -> CustomerDTO(id, firstName, lastName, email) }
 
     route("/customers") {
+        post {
+            val input = call.receiveNullable<CustomerDTO>()
+            if (input == null) {
+                call.respond(HttpStatusCode.UnprocessableEntity, "No parameter found.")
+                return@post
+            }
+
+            val customer = input.toEntity()
+            db.customerQueries.insert(customer)
+            call.respond(HttpStatusCode.Created, "A customer created successfully")
+        }
         get {
-            val customers = db.customerQueries.selectAll(mapper).executeAsList()
+            val customers = db.customerQueries.selectAll(toDto).executeAsList()
             call.respond(customers)
         }
         get("{id?}") {
@@ -21,13 +34,22 @@ fun Route.customerRouting(db: Database) {
                 "Missing id", status = HttpStatusCode.BadRequest
             )
             val id = idStr.toLong()
-            val customer: CustomerDTO? = db.customerQueries.selectById(id, mapper).executeAsOneOrNull()
+            val customer: CustomerDTO? = db.customerQueries.selectById(id, toDto).executeAsOneOrNull()
 
             if (customer == null) {
                 call.respond(HttpStatusCode.NotFound, "No customer found.")
                 return@get
             }
             call.respond(customer)
+        }
+        delete("{id?}") {
+            val idStr = call.parameters["id"] ?: return@delete call.respondText(
+                "Missing id", status = HttpStatusCode.BadRequest
+            )
+            val id = idStr.toLong()
+
+            db.customerQueries.deleteById(id)
+            call.respond("Successfully deleted.")
         }
     }
 }
